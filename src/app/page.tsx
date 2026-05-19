f5188984eca14b9f1c6489e7ff61e5b82d18c9f8
hakeem0059 <abdulhakeem@tenetlabs.uk>
2026-05-19 13:02:50 +0530

    Update logo image size in Logo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -617,7 +617,7 @@ function SectionHeading({
 
 function Logo() {
   return (
-    <img src="/logo1.png" alt="VIP School Logo" className="h-20 w-auto" />
+    <img src="/logo1.png" alt="VIP School Logo" className="h-12 w-auto max-w-[200px]" />
   );
 }
 function Arrow() {
