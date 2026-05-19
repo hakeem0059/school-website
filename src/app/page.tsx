9b63dc0711124ef5ae80c3eafdb4868c83e22dd3
hakeem0059 <abdulhakeem@tenetlabs.uk>
2026-05-19 12:54:39 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -617,7 +617,7 @@ function SectionHeading({
 
 function Logo() {
   return (
-    <img src="/logo.png" alt="VIP School Logo" className="h-10 w-auto" />
+    <img src="/logo1.png" alt="VIP School Logo" className="h-10 w-auto" />
   );
 }
 function Arrow() {
